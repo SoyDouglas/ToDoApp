6e06428554f5f9bb328cec1ed0ddb6cba7430ccd
soydouglas <soydouglass@gmail.com>
2025-06-24 20:23:23 -0500

    Add category: done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-// JS code
+
+// Add category
+
+const listUserCategories = document.getElementById("listUserCategories");
+const addCategoryButton = document.getElementById("addCategoryButton");
+let customCategories = [];
+
+addCategoryButton.addEventListener("click", () => {
+    const nameCustomCategory = prompt("Category name: ");
+    
+    if(!nameCustomCategory){
+        return;
+    }
+
+    const id = nameCustomCategory.trim().toLowerCase().replace(/\s+/g, "-");
+
+    const newCategory = {nameCustomCategory, id};
+    customCategories.push(newCategory);
+    renderCustomCategories();
+});
+
+function renderCustomCategories() {
+    listUserCategories.innerHTML = "";
+
+    customCategories.forEach(cat => {
+        const li = document.createElement("li");
+
+        li.className = "categoryItem";
+        li.dataset.cat = cat.id;
+        li.textContent = cat.nameCustomCategory;
+
+        li.addEventListener("click", () => {
+            // Change category, render tasks of the active category, etc..
+            console.log(`Change to custom category: ${cat.id}`);
+        });
+        listUserCategories.appendChild(li);
+    })
+}
+
